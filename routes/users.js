ab27dd19c2c1c17c8e29fa9f94807dade9056d22
MociW <mochixmoca@gmail.com>
2022-09-16 21:27:20 +0700

    new

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,17 @@ import {
     updateuser,
 } from "../controllers/user-controller.js";
 import { Router } from "express";
-import { verifyToken } from "../utils/verification.js";
+import { verifyToken, verifyUser } from "../utils/verification.js";
 const router = Router();
 
 router.get("/checkauthentication", verifyToken, (req, res, next) => {
     res.send("hello user");
 });
+
+router.get("/checkuse/:id", verifyUser, (req, res, next) => {
+    res.send("hello user and you can edit your account");
+});
+
 //update
 router.put("/:id", updateuser);
 //Delete
